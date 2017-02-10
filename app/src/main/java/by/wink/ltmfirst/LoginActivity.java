027d6ce8bed48e67f551e3c8b5015bdfbb40e2ca
Amine Nokra <amine.nokra@gmail.com>
2017-02-10 17:05:00 +0100

    Intent & intent filters

--- app/src/main/java/by/wink/ltmfirst/LoginActivity.java
+++ app/src/main/java/by/wink/ltmfirst/LoginActivity.java
@@ -1,6 +1,7 @@
 package by.wink.ltmfirst;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -20,22 +21,6 @@ public class LoginActivity extends Activity implements View.OnClickListener{
 
     Button loginBtn;
 
-    View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(view.getId() == R.id.login_btn){
-
-                String username = usernameET.getText().toString();
-                String password = passwordET.getText().toString();
-                if(doLogin(username,password)){
-                    Toast.makeText(LoginActivity.this,getString(R.string.login_success),Toast.LENGTH_SHORT)
-                            .show();
-                }
-            }
-
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,13 +30,14 @@ public class LoginActivity extends Activity implements View.OnClickListener{
         usernameET = (EditText) findViewById(R.id.login_username_et);
         passwordET = (EditText) findViewById(R.id.login_pwd_et);
         loginBtn = (Button) findViewById(R.id.login_btn);
-        
+
+
         loginBtn.setOnClickListener(this);
 
     }
 
 
-    private boolean doLogin(String username,String password){
+    private boolean doLogin(String username, String password){
 
         // do something
 
@@ -68,6 +54,10 @@ public class LoginActivity extends Activity implements View.OnClickListener{
             if (doLogin(username, password)) {
                 Toast.makeText(LoginActivity.this, getString(R.string.login_success), Toast.LENGTH_SHORT)
                         .show();
+                // explict intent : this Activity , ComponentName to create
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
             }
 
         }
--- app/src/main/java/by/wink/ltmfirst/MainActivity.java
+++ app/src/main/java/by/wink/ltmfirst/MainActivity.java
@@ -1,24 +1,74 @@
 package by.wink.ltmfirst;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by amine on 06/02/17.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     private static final String TAG = "MainActivity";
+
+    TextView phoneNumberTV;
+    Button callBtn,shareBtn;
+    private TextView addressTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        phoneNumberTV = (TextView)findViewById(R.id.phone_numberTV);
+        addressTV = (TextView)findViewById(R.id.address_textView);
+
+        callBtn = (Button) findViewById(R.id.call_btn);
+        shareBtn = (Button) findViewById(R.id.share_btn);
+        callBtn.setOnClickListener(this);
+        shareBtn.setOnClickListener(this);
+        findViewById(R.id.send_mail_btn).setOnClickListener(this);
         Log.d(TAG,"onCreate");
     }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.call_btn){
+            Intent intent = new Intent();
+
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("tel:"+phoneNumberTV.getText().toString());
+            intent.setData(uri);
+            startActivity(intent);
+
+        }else if(view.getId() == R.id.share_btn){
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("geo:0,0?q=" + addressTV.getText().toString());
+            intent.setData(uri);
+            startActivity(intent);
+
+        }else if(view.getId() == R.id.send_mail_btn){
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT,getString(R.string.email_text));
+            intent.setType("text/plain");
+            startActivity(intent);
+
+        }
+
+    }
+
 
 
     @Override
